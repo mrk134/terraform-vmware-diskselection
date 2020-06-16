30413418d40d743d45982138fe75662ad5141a0d
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-16 19:29:42 +0200

    Update vmware_simple_vm_variable.tf

--- vmware_simple_vm_variable.tf
+++ vmware_simple_vm_variable.tf
@@ -1,39 +1,67 @@
 variable "user" {
-    type = "string"
+    type = string
 }
 variable "password" {
-    type = "string"
+    type = string
 }
 variable "host" {
-    type = "string"
+    type = string
 }
 variable "region" {
-    type = "string"
+    type = string
 }
 variable "datastore" {
-    type = "string"
+    type = string
+    default = "vsanDatastore"
 }
 variable "cluster" {
-    type = "string"
+    type = string
+    default = "VSANCLUSTER01"
 }
 variable "network_interface" {
-    type = "string"
+    type = string
+    default = "VM Network"
 }
 variable "domain" {
-    type = "string"
+    type = string
+    default = "corp.snow.com"
 }
 variable "hostname" {
-    type = "string"
+    type = string
+    dfault = "hashisn000"
 }
 variable "templateName" {
-    type = "string"
+    type = string
+    default = "UBUNTU_TMPL"
 }
 variable "vmname" {
-    type = "string"
+    type = string
+    default = "hashisn000"
 }
 variable "foldername" {
-    type = "string"
+    type = string
+    default = "Discovered virtual machine"
 }
-variable "diskpath" {
-    type = "string"
+#variable "diskpath" {
+ #   type = string
+#}
+variable "disksize" {
+    type = string
+    default = "50"
+}
+variable "vmcpu" {
+    type = string
+    default = "2"
+}
+variable "vmmemory" {
+    type = string
+    default = "1024"
+}
+variable "vmipaddress" {
+    type = string
+    default = "10.10.0.134"
+}
+variable "vmtype" {
+    type = string
+    default = "small"
 }

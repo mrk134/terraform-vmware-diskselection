c473be49d5a87cd6a41129ca41ae6cc9a20a9eb6
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-16 19:22:25 +0200

    Update vmware_simple_vm_template.tf

--- vmware_simple_vm_template.tf
+++ vmware_simple_vm_template.tf
@@ -2,25 +2,19 @@ provider "vsphere" {
   user           = "${var.user}"
   password       = "${var.password}"
   vsphere_server = "${var.host}"
-
   allow_unverified_ssl = true
 }
-
 data "vsphere_datacenter" "dc" {
   name = "${var.region}"
 }
-
 data "vsphere_datastore" "datastore" {
   name          = "${var.datastore}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_resource_pool" "pool" {
   name          = "${var.cluster}/Resources"
 datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
-
 data "vsphere_network" "network" {
   name          = "${var.network_interface}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
@@ -30,22 +24,19 @@ data "vsphere_virtual_machine" "template" {
   name          = "${var.templateName}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
-
-## resource "vsphere_virtual_disk" "myDisk" {
-#  size         = 30
-#  vmdk_path    = "${var.diskpath}"
-#  datacenter = "${data.vsphere_datacenter.dc.name}"
-#  datastore    = "${var.datastore}"
-#  type         = "thin"
+#resource "vsphere_virtual_disk" "myDisk" {
+ # size         = "${var.disksize}"
+ # vmdk_path    = "${var.diskpath}"
+ # datacenter = "${data.vsphere_datacenter.dc.name}"
+ # datastore    = "${var.datastore}"
+ # type         = "thin"
 #}
-
 #### VM CREATION ####
 # Set vm parameters
 resource "vsphere_virtual_machine" "vm-one" {
   name                 = "${var.vmname}"
-  num_cpus             = 2
-  memory               = 512
+  num_cpus             = "${var.vmcpu}"
+  memory               = "${var.vmmemory}"
   datastore_id         = "${data.vsphere_datastore.datastore.id}"
   #host_system_id      = "${data.vsphere_host.host.id}"
   resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
@@ -54,23 +45,24 @@ resource "vsphere_virtual_machine" "vm-one" {
   folder               = "${var.region}/vm/${var.foldername}"
   # Set network parameters
   network_interface {
-    network_id         = "${data.vsphere_network.network.id}"
+    network_id = "${data.vsphere_network.network.id}"
   }
   # Use a predefined vmware template has main disk
  # disk {
-   #size ="${vsphere_virtual_disk.myDisk.size}"
+  #  size ="${var.disksize}"
    # label="demodisk1"
-   # attach = true
-   # datastore_id     = "${data.vsphere_datastore.datastore.id}"
-   # path = "${vsphere_virtual_disk.myDisk.vmdk_path}"
-   # unit_number = 1
-  #  thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    #attach = true
+    #datastore_id     = "${data.vsphere_datastore.datastore.id}"
+    #path = "${vsphere_virtual_disk.myDisk.vmdk_path}"
+    #unit_number = 1
+    #thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
  # }
 disk {
-    label="testdisk2.vmdk"
-    size = "30"
+    label="testdisk1.vmdk"
+    size = "${var.disksize}"
 thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
+
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
     customize {
@@ -79,8 +71,11 @@ thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisi
         domain    = "${var.domain}"
       }
       network_interface {
-
+	    ipv4_address = "${var.vmipaddress}"
+	    ipv4_netmask = "28"
       }
+      ipv4_gateway = "10.13.203.113"
+      dns_server_list = ["156.54.205.68","156.54.240.134"]
     }
   }
 }

97ba668a4c2c787abc32bb812b774392a4e87382
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-30 18:38:16 +0200

    Update vmware_simple_vm_template.tf

--- vmware_simple_vm_template.tf
+++ vmware_simple_vm_template.tf
@@ -84,3 +84,44 @@ thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisi
     }
   }
 }
+
+data "vsphere_tag_category" "category" {
+  name        = "general_info"
+  #cardinality = "SINGLE"
+  #description = "General Info"
+
+ #associable_types = [
+   # "VirtualMachine",
+   # "Datastore",
+  #]
+}
+
+data "vsphere_tag" "Application" {
+  name        = "Application"
+  category_id = "${data.vsphere_tag_category.category.id}"
+  description = "Apache"
+}
+
+data "vsphere_tag" "BusinessService" {
+  name        = "BusinessService"
+  category_id = "${data.vsphere_tag_category.category.id}"
+  description = "Digital App"
+}
+
+data "vsphere_tag" "CostCenter" {
+  name        = "CostCenter"
+  category_id = "${data.vsphere_tag_category.category.id}"
+  description = "Devteam"
+}
+
+data "vsphere_tag" "UserGroup" {
+  name        = "UserGroup"
+  category_id = "${data.vsphere_tag_category.category.id}"
+  description = "ITOM EMEA PLSC"
+}
+
+data "vsphere_tag" "User" {
+  name        = "User"
+  category_id = "${data.vsphere_tag_category.category.id}"
+  description = "Mark Radonic"
+}

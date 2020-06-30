5be1a58afd7c9545cd0616dc22cdc4526c34dbf4
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-30 17:03:31 +0200

    Update tags.tf

--- tags.tf
+++ tags.tf
@@ -14,3 +14,27 @@ resource "vsphere_tag" "Application" {
   category_id = "${vsphere_tag_category.category.id}"
   description = "Apache"
 }
+
+resource "vsphere_tag" "BusinessService" {
+  name        = "BusinessService"
+  category_id = "${vsphere_tag_category.category.id}"
+  description = "Digital App"
+}
+
+resource "vsphere_tag" "CostCenter" {
+  name        = "CostCenter"
+  category_id = "${vsphere_tag_category.category.id}"
+  description = "Devteam"
+}
+
+resource "vsphere_tag" "UserGroup" {
+  name        = "UserGroup"
+  category_id = "${vsphere_tag_category.category.id}"
+  description = "ITOM EMEA PLSC"
+}
+
+resource "vsphere_tag" "User" {
+  name        = "User"
+  category_id = "${vsphere_tag_category.category.id}"
+  description = "Mark Radonic"
+}
